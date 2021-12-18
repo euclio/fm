ad6293267419cd9757de60dc56eb5aad369daeac
Andy Russell <arussell123@gmail.com>
2021-12-18 13:33:27 -0500

    arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 use std::env;
+use std::fs;
+use std::path::PathBuf;
 
-use relm4::gtk;
-use relm4::RelmApp;
+use anyhow::Result;
+use clap::Parser;
+use log::*;
+use relm4::{gtk, RelmApp};
 
-use fm4::AppModel;
+use fm::AppModel;
 
-fn main() {
+/// A paned file manager with automatic preview.
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+    /// The file or directory to open.
+    #[clap(default_value = ".")]
+    file: PathBuf,
+}
+
+fn main() -> Result<()> {
     env_logger::init();
+
+    let args = Args::parse();
+
+    info!("running with arguments: {:?}", args);
 
     // Call `gtk::init` manually because we instantiate GTK types in our model.
     gtk::init().unwrap();
 
-    let model = AppModel::new(&env::current_dir().unwrap());
+    let model = AppModel::new(&fs::canonicalize(args.file)?);
     let app = RelmApp::new(model);
     app.run();
+
+    Ok(())
 }
