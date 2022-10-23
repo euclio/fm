b05d8a3c6b718501c7f9d1d37ff3fcd9de109ef1
Andy Russell <arussell123@gmail.com>
2022-10-23 17:19:01 -0400

    wip

---
+++ src/mount.rs
@@ -0,0 +1,93 @@
+//! Dialog component for mounting a new mountable.
+
+use gtk::prelude::*;
+use relm4::prelude::*;
+
+#[derive(Debug)]
+pub struct Mount {
+    visible: bool,
+}
+
+#[derive(Debug)]
+pub enum MountMsg {
+    /// Mount a new mountable.
+    Mount,
+
+    /// User clicked an action.
+    Response(gtk::ResponseType),
+
+    /// Close the dialog.
+    Close,
+}
+
+#[relm4::component(pub)]
+impl SimpleComponent for Mount {
+    type Init = ();
+    type Input = MountMsg;
+    type Output = ();
+    type Widgets = MountWidgets;
+
+    view! {
+        dialog = gtk::Dialog::builder()
+            .title("Connect to remote folder")
+            .use_header_bar(gtk::Settings::default().unwrap().is_gtk_dialogs_use_header() as i32)
+            .build() {
+
+            #[watch]
+            set_visible: model.visible,
+
+            #[chain(add_css_class("suggested-action"))]
+            add_button: ("Connect", gtk::ResponseType::Accept),
+
+            add_button: ("Cancel", gtk::ResponseType::Cancel),
+
+            gtk::Entry {
+
+            },
+
+            gtk::Entry {
+
+            },
+
+            gtk::PasswordEntry {
+
+            },
+
+            connect_response[sender] => move |_, response| {
+                sender.input(MountMsg::Response(response));
+            },
+
+            connect_close_request[sender] => move |_| {
+                sender.input(MountMsg::Close);
+                gtk::Inhibit(true)
+            },
+        }
+    }
+
+    fn init(init: (), root: &Self::Root, sender: ComponentSender<Self>) -> ComponentParts<Self> {
+        let model = Mount {
+            visible: false,
+        };
+
+        let accept_button = gtk::Button::new();
+
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, msg: Self::Input, sender: ComponentSender<Self>) {
+        match msg {
+            MountMsg::Mount => {
+                self.visible = true;
+            },
+            MountMsg::Response(response) if response == gtk::ResponseType::Cancel => {
+                self.visible = false;
+            },
+            MountMsg::Close => {
+                self.visible = false;
+            },
+            _ => (),
+        }
+    }
+}
